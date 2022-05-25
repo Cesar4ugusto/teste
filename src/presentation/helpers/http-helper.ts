2561d307d3914176ae0be86a5a9c94e2245b701b
César Augusto <cesarapolidorio@gmail.com>
2022-05-25 18:09:04 -0300

    feat: move all http protocols to a helper method

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { ServerError } from "../errors";
+import { HttpResponse } from "../protocols";
+
+export const badRequest = (error: Error): HttpResponse => ({
+    statusCode: 400,
+    body: error,
+});
+
+export const notFound = (error: Error): HttpResponse => ({
+    statusCode: 404,
+    body: error,
+});
+
+export const serverError = (): HttpResponse => ({
+    statusCode: 500,
+    body: new ServerError(),
+});
+
+export const created = (data: any): HttpResponse => ({
+    statusCode: 201,
+    body: data,
+});
